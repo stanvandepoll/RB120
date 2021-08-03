478279178aa7cb389fb67a876028ce58c4f4c2a9
Stan van de Poll <swvandepoll@gmail.com>
2021-08-03 10:54:38 +0200

    Complete rps walkthrough

--- lesson2/rps.rb
+++ lesson2/rps.rb
@@ -3,14 +3,19 @@ class RPSGame
 
   def initialize
     @human = Player.new
-    @computer = Player.new
+    @computer = Player.new(:computer)
   end
 
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
+
     display_goodbye_message
   end
 
@@ -21,15 +26,85 @@ class RPSGame
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
   end
+
+  def display_winner
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts "#{human.name} won!" if computer.move == 'scissors'
+      puts "#{computer.name} won!" if computer.move == 'paper'
+    when 'paper'
+      puts "It's a tie!" if computer.move == 'paper'
+      puts "#{human.name} won!" if computer.move == 'rock'
+      puts "#{computer.name} won!" if computer.move == 'scissors'
+    when 'scissors'
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts "#{human.name} won!" if computer.move == 'paper'
+      puts "#{computer.name} won!" if computer.move == 'rock'
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+
+      puts "Sorry, must be y or n."
+    end
+
+    answer == 'y'
+  end
 end
 
 class Player
-  def initialize
-    # maybe a "name"? what about a "move"?
+  attr_accessor :move, :name
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
+    set_name
+  end
+
+  def set_name
+    if human?
+      chosen_name = nil
+      loop do
+        puts "What's your name?"
+        chosen_name = gets.chomp
+        break unless chosen_name.empty?
+
+        puts "Sorry, must enter a value."
+      end
+
+      self.name = chosen_name
+    else
+      self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+    end
   end
 
   def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, or scissors:"
+        choice = gets.chomp
+        break if %w(rock paper scissors).include?(choice)
 
+        puts "Sorry, invalid choice."
+      end
+      self.move = choice
+    else
+      self.move = %w(rock paper scissors).sample
+    end
+  end
+
+  def human?
+    @player_type == :human
   end
 end
 
