1e90a860453bba144a5ce0f65a3ecf43287453f9
Stan van de Poll <swvandepoll@gmail.com>
2021-08-06 16:14:58 +0200

    Implement display board method

--- lesson5/ttt.rb
+++ lesson5/ttt.rb
@@ -1,4 +1,50 @@
+class Board
+  def initialize
+    @grid = initialize_grid
+  end
+
+  def initialize_grid
+    fresh_grid = {}
+    1.upto(9) do |square_number|
+      fresh_grid[square_number] = Square.new
+    end
+    fresh_grid
+  end
+
+  def [](integer)
+    @grid[integer]
+  end
+end
+
+class Square
+  def initialize
+    @state = ''
+  end
+
+  def to_s
+    @state
+  end
+end
+
+class Player
+  def initialize(name, marker, type = :computer)
+    @name = name
+    @type = type
+    @marker = marker
+  end
+
+  def mark
+
+  end
+end
+
 class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
   def play
     display_welcome_message
     loop do
@@ -22,41 +68,21 @@ class TTTGame
   end
 
   def display_board
-
+    %(
+  (1)|(2)|(3)
+   #{board[1]} | #{board[2]} | #{board[3]}
+     |   |
+  ---+---+---
+  (4)|(5)|(6)
+   #{board[4]} | #{board[5]} | #{board[6]}
+     |   |
+  ---+---+---
+  (7)|(8)|(9)
+   #{board[7]} | #{board[8]} | #{board[9]}
+     |   |
+  )
   end
 end
 
 game = TTTGame.new
 game.play
-
-class Board
-  def initialize
-    @grid = initialize_grid
-  end
-
-  def initialize_grid
-    fresh_grid = {}
-    1.upto(9) do |square_number|
-      fresh_grid[square_number] = Square.new
-    end
-    fresh_grid
-  end
-end
-
-class Square
-  def initialize
-    @state = ''
-  end
-end
-
-class Player
-  def initialize(name, marker, type = :computer)
-    @name = name
-    @type = type
-    @marker = marker
-  end
-
-  def mark
-
-  end
-end
