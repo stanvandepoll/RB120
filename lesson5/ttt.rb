0b4d9c91b380ea6ee0b00e41f4f31cb01d68f0e7
Stan van de Poll <swvandepoll@gmail.com>
2021-08-06 15:52:43 +0200

    Add game engine class

--- lesson5/ttt.rb
+++ lesson5/ttt.rb
@@ -1,3 +1,34 @@
+class TTTGame
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      first_player_moves
+      break if someone_won? || board_full?
+
+      second_player_moves
+      break if someone_won? || board_full?
+    end
+    display_result
+    display_goodbye_message
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+  end
+
+  def display_goodbye_method
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+
+  end
+end
+
+game = TTTGame.new
+game.play
+
 class Board
   def initialize
     @grid = initialize_grid
@@ -26,10 +57,6 @@ class Player
   end
 
   def mark
-
-  end
-
-  def play
 
   end
 end
