f816e90e86a9fb68eb18e23682617e719ee36162
Stan van de Poll <swvandepoll@gmail.com>
2021-08-20 10:59:35 +0200

    first stab at deck and card class implementations

--- lesson5/21.rb
+++ lesson5/21.rb
@@ -1,4 +1,20 @@
+module Hand
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
 class Player
+  include Hand
+
   def initialize
     # what would the "data" or "states" of a Player object entail?
     # maybe cards? a name?
@@ -19,45 +35,64 @@ class Player
 end
 
 class Dealer
+  include Hand
+
   def initialize
     # seems like very similar to Player... do we even need this?
   end
-
-  def deal
-    # does the dealer or the deck deal?
-  end
-
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
-  end
-end
-
-class Participant
-  # what goes in here? all the redundant behaviors from Player and Dealer?
 end
 
 class Deck
+  SUITS = %w(H D S C)
+  FACES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  DECK = SUITS.product(FACES)
+
   def initialize
-    # obviously, we need some data structure to keep track of cards
-    # array, hash, something else?
+    @cards = DECK.shuffle.map do |suit, face|
+      Card.new(suit, face)
+    end
   end
 
   def deal
-    # does the dealer or the deck deal?
+    @cards.pop
   end
 end
 
 class Card
-  def initialize
-    # what are the "states" of a card?
+  SUITS_DESCRIPTORS = { 'H' => 'Hearts', 'D' => 'Diamonds', 'S' => 'Spades', 'C' => 'Clubs' }
+  FACES_DESCRIPTORS = { 'J' => 'Jack', 'Q' => 'Queen', 'K' => 'King', 'A' => 'Ace' }
+
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+  end
+
+  def to_s
+    "The #{face} of #{suit}"
+  end
+
+  def face
+    FACES_DESCRIPTORS[@face] || @face
+  end
+
+  def suit
+    SUITS_DESCRIPTORS[@suit]
+  end
+
+  def ace?
+    face == 'Ace'
+  end
+
+  def king?
+    face == 'King'
+  end
+
+  def queen?
+    face == 'Queen'
+  end
+
+  def jack?
+    face == 'Jack'
   end
 end
 
